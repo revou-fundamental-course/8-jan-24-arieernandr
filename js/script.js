ee4e2a485b357a8c46b59ee00e9e39cf59c3eaf8
arieernandr <83866243+arieernandr@users.noreply.github.com>
2024-01-18 21:06:29 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var value = document.getElementById ('angka');
+var hasil = document.getElementById ('hasil');
+var reset = document.getElementById ('reset');
+function hitung(value){
+    var proses;
+    switch (value) {
+        case'x' :
+        proses = parseInt(angka.value) * parseInt(angka.value);
+        hasil.innerHTML = proses;
+        break;
+    }
+function reset(value){
+    document.getElementById="";
+}
+
+}
